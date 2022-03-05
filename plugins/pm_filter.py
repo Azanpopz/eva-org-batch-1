48c5a24e0c8d4043f8bac302f910759d69a4e891
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-03-05 13:08:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,7 +407,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('📂 Our Channel', url='https://t.me/SS_Linkz')
+            InlineKeyboardButton('📂 Our Channel', url='https://t.me/nasrani_update')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -438,7 +438,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🎥 Movie Group', url='https://t.me/NetflixMovies_sslinkz'),
+            InlineKeyboardButton('🎥 Movie Group', url='https://t.me/nasrani_update'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
