6659b8282501422647023a8f81b24148f6e7fb6a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-03-07 19:13:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -165,7 +165,7 @@ async def start(client, message):
         sts = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
-        caption=f_caption,
+        
         parse_mode="html",
                     reply_markup=InlineKeyboardMarkup(
                         [
