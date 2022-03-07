749a26b31bef831589e18d3379653011e12da0a0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-03-07 22:54:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -194,7 +194,7 @@ async def start(client, message):
                                 InlineKeyboardButton('🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
                             ],
                             [
-                                InlineKeyboardButton('🧩𝐆𝐨𝐨𝐠𝐥𝐞🧩', url=https://imdb.com"),
+                                InlineKeyboardButton('🧩𝐆𝐨𝐨𝐠𝐥𝐞🧩', url="https://imdb.com"),
                                 InlineKeyboardButton('☘𝐈𝐦𝐝𝐛☘', url="https://imdb.com")
                             ]                            
                         ]
