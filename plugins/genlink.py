82e7a4d22f298eb7ba43e3776a89837be4ddbf87
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-03-08 11:31:26 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -77,7 +77,7 @@ async def gen_link_batch(bot, message):
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-        return await sts.edit(f"Here is your link https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}")
+        return await sts.edit(f"Here is your link <code> /p \n [😌](buttonurl:// https://t.me/{temp.U_NAME}?start=DSTORE-{b_64} </code>")
 
     FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
 
@@ -122,7 +122,7 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
+    await sts.edit(f"Here is your link\nContains <code> /p \n [😌](buttonurl://`{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id} </code>")
    
     await client.send_cached_media(
                     chat_id=message.from_user.id,
