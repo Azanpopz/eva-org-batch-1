fad69e45f9c5c420256d120eec2c5e07c041bcbc
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-03-22 01:59:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -808,7 +808,7 @@ async def advantage_spell_chok(msg):
      
     
     k = await query.answer("\n\n♻️𝐇𝐞𝐲𝐚 𝐚𝐥𝐥 𝐚𝐫𝐞 𝐬𝐢𝐦𝐩𝐥𝐞 𝐭𝐨 𝐮𝐬𝐞 𝐀𝐥𝐥 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐫𝐞 𝐦𝐞𝐧𝐭𝐢𝐨𝐧𝐞𝐝 𝐚𝐛𝐨𝐯𝐞.♻️ ", show_alert=True)           
-    reply_markup=InlineKeyboardMarkup(btn))
+    reply_markup=InlineKeyboardMarkup(btn)
     return k
 
 
