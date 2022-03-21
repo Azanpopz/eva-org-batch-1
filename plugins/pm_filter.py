1f02a1571404609730c1fe48cde02bb49e987758
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-03-21 20:48:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -439,7 +439,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "Tex😂😂t_ex":
         await update.answer("\n\n♻️𝐇𝐞𝐲𝐚 𝐚𝐥𝐥 𝐚𝐫𝐞 𝐬𝐢𝐦𝐩𝐥𝐞 𝐭𝐨 𝐮𝐬𝐞 𝐀𝐥𝐥 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐚𝐫𝐞 𝐦𝐞𝐧𝐭𝐢𝐨𝐧𝐞𝐝 𝐚𝐛𝐨𝐯𝐞.♻️ ", show_alert=True)
     elif query.data == "about":
-        reply_markup=InlineKeyboardMatkup([[InlineKeyboardButton("ok🥰🥰", "alert")]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ok🥰🥰", "alert")]])
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
             reply_markup=reply_markup,
